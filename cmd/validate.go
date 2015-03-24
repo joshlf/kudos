6db02de2cea52600dd56f4438ca3285fe38391d2
Joshua Liebow-Feeser <jliebowf@cs.brown.edu>
2015-03-24 14:44:21 -0400

    Added "validate" long description

--- cmd/validate.go
+++ cmd/validate.go
@@ -9,7 +9,16 @@ import (
 var cmdValidate = &cobra.Command{
 	Use:   "validate",
 	Short: "validate the configuration",
-	Long:  ``,
+	Long: `Perform all of the steps necessary
+to determine kudos' configuration,
+reporting any errors encountered.
+This includes reading environment
+variables, command line flags, and
+configuration files. Validate will
+check not only the syntax of these,
+but will also perform sanity checks
+to make sure that the configuration
+makes sense.`,
 }
 
 func init() {
