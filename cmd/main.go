43132fac146ff4aea815820899790709a863941c
Eli Rosenthal <eli_rosenthal@brown.edu>
2015-03-24 18:50:18 -0400

    fixing history stuff

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,7 @@ import "github.com/spf13/cobra"
 var cmdMain = &cobra.Command{
 	Use:   "kudos",
 	Short: "kudos is a simple grading system",
-	Long: `kudos is a simple grading system made out of love and frustration by m, ezr, 
+	Long: `kudos is a simple grading system made out of love and frustration by m, ezr,
 and jliebowf`,
 }
 
