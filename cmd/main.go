97b2ffdfd51f75e8a462598bf5f180a160a1f85b
Joshua Liebow-Feeser <jliebowf@cs.brown.edu>
2015-03-24 14:52:09 -0400

    Grammatical edit

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,7 @@ import "github.com/spf13/cobra"
 var cmdMain = &cobra.Command{
 	Use:   "kudos",
 	Short: "kudos is a simple grading system",
-	Long:  `Made out of love and frustration by m, ezr, jliebowf`,
+	Long:  `Made out of love and frustration by m, ezr, and jliebowf`,
 }
 
 func main() {
