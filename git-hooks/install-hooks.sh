fa9de68e7b60a0449300b2752bc84b56978ebf1d
Joshua Liebow-Feeser <hello@synful.io>
2015-03-25 05:26:09 -0400

    Added git hooks

---
+++ git-hooks/install-hooks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# source: http://stackoverflow.com/a/957978/836390
+ROOT=$(git rev-parse --show-toplevel) || exit 1
+
+if [ "$ROOT" == "" ]; then
+	echo "`git rev-parse --show-toplevel` returned empty root path" >&2
+	exit 1
+fi
+
+cd $ROOT/.git/hooks || exit 1
+
+for fpath in ../../git-hooks/*; do
+	fname="$(echo $fpath | sed -e 's/\/$//' | rev | cut -d / -f 1 | rev)"
+	if [ "$fname" != "install-hooks.sh" ]; then
+		ln -s "$fpath" "$fname" || exit 1
+	fi
+done
