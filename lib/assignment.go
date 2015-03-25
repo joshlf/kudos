c0355e98f7c71c9696f4c19c4965c143371103fc
Eli Rosenthal <eli_rosenthal@brown.edu>
2015-03-25 00:08:49 -0400

    added fromfile function for toml config files

--- lib/assignment.go
+++ lib/assignment.go
@@ -1,6 +1,11 @@
 package assignment
 
-import "time"
+import (
+	"io/ioutil"
+	"time"
+
+	"github.com/BurntSushi/toml"
+)
 
 func timeparse(text string) (time.Time, error) {
 	return time.Parse("Jan 2, 2006 at 3:04pm (MST)", text)
@@ -45,3 +50,19 @@ func (d *duration) UnmarshalText(text []byte) error {
 }
 
 //end taken from example
+
+func AsgnFromFile(file string) (AssignSpec, error) {
+	var fileText []byte
+	var err error
+	var res AssignSpec
+
+	if fileText, err = ioutil.ReadFile(file); err != nil {
+		return AssignSpec{}, err
+	}
+
+	if _, err = toml.Decode(string(fileText), &res); err != nil {
+		return AssignSpec{}, err
+	}
+
+	return res, nil
+}
