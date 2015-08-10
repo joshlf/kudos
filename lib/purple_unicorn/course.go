d78b856a26643cf084009d8ecc800b1e0a91946a
Eli Rosenthal <ezr@cs.brown.edu>
2015-08-09 17:49:47 -0700

    Added getters, setters for course struct

--- lib/purple_unicorn/course.go
+++ lib/purple_unicorn/course.go
@@ -7,18 +7,22 @@ import (
 	"strings"
 )
 
-type Group string
-type User string
-
 type Course struct {
 	code         Code
 	name         string
 	description  string
 	taGroup      *Group
-	tas          []User //TODO:  []TA?, how should this be represented
+	tas          []User
 	studentGroup *Group
-	handinMethod string
+	handinMethod *string
 }
+
+// supported handin methods
+var handinMethods = map[string]bool{
+	"facl": true,
+}
+
+type Group string
 
 func (g Group) Validate() error {
 	group := string(g)
@@ -41,37 +45,53 @@ func (g Group) Validate() error {
 	}
 }
 
+func (g Group) MustValidate() {
+	err := g.Validate()
+	if err != nil {
+		panic(err)
+	}
+}
+
+type User string
+
 //TODO: we probably want to have separate Group and TA types to do a validate on
 func (u User) Validate() error {
 	//TODO nyi
 	return nil
 }
 
+func (u User) MustValidate() {
+	err := u.Validate()
+	if err != nil {
+		panic(err)
+	}
+}
+
 func (c *Course) Validate() error {
 	var errs ErrList
 
 	if err := c.code.Validate(); err != nil {
-		errs = append(errs, err)
+		errs.Add(err)
 	}
+
 	if c.taGroup != nil {
 		if len(*c.taGroup) == 0 {
-			errs = append(errs,
-				fmt.Errorf("Ta Group must not be the empty string; if you don't want a ta group, remove the line"))
+			errs.Add(fmt.Errorf("Ta Group must not be the empty string; if you don't want a ta group, remove the line"))
 			goto students
 		}
 		if err := c.taGroup.Validate(); err != nil {
-			errs = append(errs, fmt.Errorf("Ta group invalid: %v", err))
+			errs.Add(fmt.Errorf("Ta group invalid: %v", err))
 		}
 	}
 
 students:
 	if c.studentGroup != nil {
 		if len(*c.studentGroup) == 0 {
-			errs = append(errs, fmt.Errorf("Student Group must not be the empty string; if you don't want a ta group, remove the line"))
+			errs.Add(fmt.Errorf("Student Group must not be the empty string; if you don't want a ta group, remove the line"))
 			goto studentList
 		}
 		if err := c.studentGroup.Validate(); err != nil {
-			errs = append(errs, fmt.Errorf("Student group invalid: %v", err))
+			errs.Add(fmt.Errorf("Student group invalid: %v", err))
 		}
 	}
 
@@ -79,15 +99,98 @@ studentList:
 	var localErrList ErrList
 	for _, ta := range c.tas {
 		if err := ta.Validate(); err != nil {
-			localErrList = append(errs, err)
+			localErrList.Add(err)
 		}
 	}
 	if len(localErrList) > 0 {
-		errs = append(errs, localErrList)
+		errs.Add(localErrList)
 	}
+	if c.handinMethod != nil && !handinMethods[*c.handinMethod] {
+		errs.Add(fmt.Errorf("Unsupported handin method: %v", *c.handinMethod))
+	}
+
 	if len(errs) > 0 {
 		return fmt.Errorf("Error validating Course config:\n%v", errs)
 	}
 
 	return nil
+}
+
+func (c *Course) MustValidate() {
+	err := c.Validate()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (c *Course) SetTaGroup(ta *Group) (old *Group, err error) {
+	oTa := c.taGroup
+	c.SetTaGroupNoValidate(ta)
+	return oTa, c.Validate()
+}
+
+func (c *Course) SetTaGroupNoValidate(ta *Group) {
+	c.taGroup = ta
+}
+
+func (c *Course) SetStudentGroup(s *Group) (old *Group, err error) {
+	o := c.studentGroup
+	c.SetStudentGroupNoValidate(s)
+	return o, c.Validate()
+}
+
+func (c *Course) SetStudentGroupNoValidate(student *Group) {
+	c.studentGroup = student
+}
+func (c *Course) SetCode(co Code) (old Code, err error) {
+	oldCode := c.code
+	c.SetCodeNoValidate(co)
+	return oldCode, c.Validate()
+}
+
+func (c *Course) SetCodeNoValidate(co Code) {
+	c.code = co
+}
+
+func (c *Course) SetName(n string) (old string, err error) {
+	oldName := c.name
+	c.SetNameNoValidate(n)
+	return oldName, c.Validate()
+}
+
+func (c *Course) SetNameNoValidate(n string) {
+	c.name = n
+}
+
+func (c *Course) SetDescription(d string) (old string, err error) {
+	oldD := c.description
+	c.SetDescriptionNoValidate(d)
+	return oldD, c.Validate()
+}
+
+func (c *Course) SetDescriptionNoValidate(d string) {
+	c.description = d
+}
+
+func (c *Course) AddTA(ta User) error {
+	c.AddTANoValidate(ta)
+	if err := c.Validate(); err != nil {
+		c.tas = c.tas[:len(c.tas)-2]
+		return err
+	}
+	return nil
+}
+
+func (c *Course) AddTANoValidate(ta User) {
+	c.tas = append(c.tas, ta)
+}
+
+func (c *Course) SetHandinMethod(h *string) (old *string, err error) {
+	o := c.handinMethod
+	c.SetHandinMethodNoValidate(h)
+	return o, c.Validate()
+}
+
+func (c *Course) SetHandinMethodNoValidate(h *string) {
+	c.handinMethod = h
 }
