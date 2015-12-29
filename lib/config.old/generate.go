e1548497adb5223577899a992e0f160ac6528af5
Joshua Liebow-Feeser <hello@joshlf.co>
2015-12-29 00:10:15 -0500

    lib/config.old: Removed

--- lib/config.old/generate.go
+++
@@ -1,4 +0,0 @@
-package config
-
-//go:generate mkdir -p internal
-//go:generate go-bindata -o internal/bindata.go -pkg internal example/...
--- lib/config.old/util.go
+++
@@ -1,9 +0,0 @@
-package config
-
-func mapFromProbs(p []Problem) map[string]Problem {
-	m := make(map[string]Problem)
-	for _, pp := range p {
-		m[pp.Code] = pp
-	}
-	return m
-}
